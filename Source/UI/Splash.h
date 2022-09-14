92d8b971ed354ff772048d7dac0c00ae14a10abd
Arboreal Audio <arborealaudio@protonmail.com>
2022-09-13 21:42:20 -0400

    clean up splash code & change behavior add plugin format to splash

--- Source/UI/Splash.h
+++ Source/UI/Splash.h
@@ -24,31 +24,77 @@ struct Splash : Component
         Rectangle<float> bounds{ 12.f, 12.f, 66.f, 65.f };
         logo->drawWithin(g, bounds, RectanglePlacement::centred, 1.f);
 
-        blur(g);
+        if (needBlur) {
+            gin::applyStackBlur(img, 35);
+            needBlur = false;
+        }
+
+        if (splashOn) {
+            g.fillAll(Colours::black.withAlpha(0.33f));
+            g.drawImage(img, clipArea, RectanglePlacement::centred);
+            g.setColour(Colours::darkslategrey.withAlpha(0.25f));
+            g.fillRect(clipArea);
+            g.setColour(Colour(0xa7a7a7a7));
+            g.drawRoundedRectangle(clipArea, 5.f, 1.5f);
+
+            if (clipArea.reduced(50.f).withTrimmedBottom(70.f).contains(getMouseXYRelative().toFloat()))
+            {
+                g.setColour(Colours::darkslategrey);
+                g.fillRoundedRectangle(clipArea.reduced(50.f).withTrimmedBottom(70.f), 5.f);
+                repaint();
+                setMouseCursor(MouseCursor::PointingHandCursor);
+                if (isMouseButtonDown()) {
+                    URL("https://arborealaudio.com").launchInDefaultBrowser();
+                    splashOn = false;
+                    repaint();
+                    return;
+                }
+            }
+            else
+                setMouseCursor(MouseCursor::NormalCursor);
+
+            g.setColour(Colour(0xa7a7a7a7));
+
+            logo->drawWithin(g, clipArea.reduced(50.f), RectanglePlacement::yTop, 1.f);
+
+            g.setFont(getCustomFont(FontStyle::Regular).withHeight(16.f));
+            Time time(Time::getCurrentTime());
+            g.drawFittedText("v" + String(ProjectInfo::versionString) + 
+                "\n" + AudioProcessor::getWrapperTypeDescription(currentWrapper) +
+                "\n(c) Arboreal Audio " + String(time.getYear()),
+                clipArea.withTrimmedTop(150.f).toNearestInt(),
+                Justification::centred, 3, 1.f);
+        }
     }
 
     bool hitTest(int x, int y) override
     {
-        if (logo->getBounds().contains((float)x, (float)y) && onLogoClick != nullptr && !splashOn &&
+        /* left click on logo */
+        if (logo->getBounds().contains((float)x, (float)y) && onLogoClick && !splashOn &&
             ModifierKeys::currentModifiers.isLeftButtonDown())
         {
             onLogoClick();
             return true;
         }
+        /* mouse inside splash */
         else if (splashOn && clipArea.contains((float)x, (float)y))
         {
             if (ModifierKeys::currentModifiers.isLeftButtonDown()) {
                 if (clipArea.reduced(50.f).withTrimmedBottom(70.f).contains(x, y))
                     return true;
-                else {
-                    splashOn = false;
-                    repaint();
-                }
             }
             return true;
         }
+        /* mouse outside splash and logo bounds */
         else if (splashOn && !clipArea.contains((float)x, (float)y) && !logo->getBounds().contains((float)x, (float)y))
+        {
+            if (ModifierKeys::currentModifiers.isLeftButtonDown()) {
+                splashOn = false;
+                repaint();
+                return true;
+            }
             return true;
+        }
         else
             return false;
     }
@@ -66,40 +112,9 @@ struct Splash : Component
         repaint();
     }
 
-    inline void blur(Graphics& g) noexcept
+    void setPluginWrapperType(AudioProcessor::WrapperType type)
     {
-        if (needBlur) {
-            gin::applyStackBlur(img, 35);
-            needBlur = false;
-        }
-
-        if (splashOn) {
-            g.fillAll(Colours::black.withAlpha(0.33f));
-            g.drawImage(img, clipArea, RectanglePlacement::centred);
-            g.setColour(Colours::darkslategrey.withAlpha(0.25f));
-            g.fillRect(clipArea);
-            g.setColour(Colour(0xa7a7a7a7));
-            g.drawRoundedRectangle(clipArea, 5.f, 1.5f);
-
-            logo->drawWithin(g, clipArea.reduced(50.f), RectanglePlacement::yTop, 1.f);
-
-            g.setFont(getCustomFont(FontStyle::Regular).withHeight(16.f));
-            Time time(Time::getCurrentTime());
-            g.drawFittedText("v" + String(ProjectInfo::versionString) + 
-                "\n(c) Arboreal Audio " + String(time.getYear()),
-                clipArea.withTrimmedTop(150.f).toNearestInt(),
-                Justification::centred, 3, 1.f);
-
-            if (clipArea.reduced(50.f).withTrimmedBottom(70.f).contains(getMouseXYRelative().toFloat())) {
-                setMouseCursor(MouseCursor::PointingHandCursor);
-                if (isMouseButtonDown()) {
-                    URL("https://arborealaudio.com").launchInDefaultBrowser();
-                    return;
-                }
-            }
-            else
-                setMouseCursor(MouseCursor::NormalCursor);
-        }
+        currentWrapper = type;
     }
 
     std::function<void()> onLogoClick;
@@ -112,6 +127,8 @@ private:
     Rectangle<float> clipArea{ 250.f, 90.f, 225.f, 300.f };
 
     Image img;
+
+    AudioProcessor::WrapperType currentWrapper;
 
     std::unique_ptr<Drawable> logo;
 };
