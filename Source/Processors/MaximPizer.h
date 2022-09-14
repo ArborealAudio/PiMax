90723ad1266d8e967e94953900e8f541640c6032
Arboreal Audio <arborealaudio@protonmail.com>
2022-09-13 21:43:13 -0400

    avoid reading from atomics in process call

--- Source/Processors/MaximPizer.h
+++ Source/Processors/MaximPizer.h
@@ -7,13 +7,11 @@ class MaximPizer
 	AudioProcessorValueTreeState& apvts;
 
 	std::atomic<float>* curve, *clip, *type, *boost;
+    float curve_m = 0.f;
+    int clip_m = 0, type_m = 0;
+    bool boost_m = false;
 
-#if USE_SIMD_SAT
-	using Vec2 = dsp::SIMDRegister<float>;
-	Vec2 x_n1[2]{ 0.0, 0.0 }, x_n2[2]{ 0.0, 0.0 }, y_n1[2]{ 0.0, 0.0 };
-#else
-	double x_n1[2]{ 0.0, 0.0}, x_n2[2]{ 0.0, 0.0 }, y_n1[2]{ 0.0, 0.0 };
-#endif
+	double x_n1[2]{0.0}, x_n2[2]{0.0}, y_n1[2]{0.0};
 
 public:
 
@@ -27,13 +25,16 @@ public:
 
 	void prepare()
 	{
-		x_n1[0] = 0.0;
-		x_n1[1] = 0.0;
-		x_n2[0] = 0.0;
-		x_n2[1] = 0.0;
-		y_n1[0] = 0.0;
-		y_n1[1] = 0.0;
+		x_n1[0] = x_n1[1] = x_n2[0] = x_n2[1] = y_n1[0] = y_n1[1] = 0.0;
 	}
+
+    void loadAtomics()
+    {
+        curve_m = curve->load();
+        clip_m = clip->load();
+        type_m = type->load();
+        boost_m = boost->load();
+    }
 
 	template <typename T>
 	void process(const dsp::ProcessContextReplacing<T>& context)
@@ -49,88 +50,16 @@ public:
 		}
 	}
 
-	//template <typename T>
-	//inline dsp::SIMDRegister<T> processSample(int channel, dsp::SIMDRegister<T> xn) noexcept
-	//{
-	//	dsp::SIMDRegister<T> yn, x1, x2;
-	//	double k = *curve;
-	//	auto xPi = xn * halfPi;
-
-	//	/*DC offset for asym (need to ramp offset incase of switch)*/
-	//	xn += *type == 1 ? 0.2 : 0.0;
-
-	//	using xs = xsimd::batch<T>;
-
-	//	auto sum = xsimd::add(xsimd::abs((xs)xn.value), xsimd::abs((xs)x_n2[channel].value));
-	//	sum = xsimd::div(sum, (xs)2.0);
-	//	const auto a = (Vec2)jlimit((xs)0.0, (xs)1.0, sum);
-
-	//	auto g_n1 = dsp::SIMDRegister<T>::greaterThan(xn.value, (T)-1.0);
-	//	auto l_1 = dsp::SIMDRegister<T>::lessThan(xn.value, (T)1.0);
-	//	auto g_0 = dsp::SIMDRegister<T>::greaterThanOrEqual(xn.value, (T)0.0);
-
-	//	if (k > 1.0) {
-	//		const auto j = (Vec2)xsimd::pow((xs)k, xsimd::acos((xs)xn.value * (xs)xn.value)) & g_n1 & l_1;
-	//		auto xk = xsimd::pow((xs)xn.value, (xs)k);
-	//		x1 = (Vec2)xsimd::sin((xs)xPi.value * (xs)j) & g_n1 & l_1;
-	//		x2 = ((Vec2)xsimd::sin((xs)halfPi * xk) & g_n1 & l_1 & g_0) +
-	//			((Vec2)-xsimd::sin((xs)halfPi * -xk) & g_n1 & l_1 & ~g_0);
-	//		yn = (Vec2)x1 * ((Vec2)1.0 - a) + x2 * a & g_n1 & l_1;
-	//	}
-	//	else if (k < 1.0) {
-	//		const auto k_r = (1.0 / k);
-	//		const auto j = (Vec2)xsimd::pow((xs)k_r, xsimd::acos((xs)xn.value * (xs)xn.value)) & g_n1 & l_1;
-	//		auto xk = xsimd::pow((xs)xn.value, (xs)k_r);
-	//		x1 = (Vec2)xsimd::sin((xs)xPi.value * (xs)j) & g_n1 & l_1;
-	//		x2 = ((Vec2)xsimd::sin((xs)halfPi * xk) & l_1 & g_0) +
-	//			((Vec2)-xsimd::sin((xs)halfPi * -xk) & g_n1 & ~g_0);
-	//		yn = (Vec2)x1 * a + x2 * ((Vec2)1.0 - a) & g_n1 & l_1;
-	//	}
-	//	else
-	//		yn = (Vec2)xsimd::sin((xs)xPi.value) & g_n1 & l_1;
-
-	//	if (*clip == 1) {
-	//		yn = ((Vec2)1.0 & ~l_1) +
-	//			((Vec2)-1.0 & ~g_n1);
-	//	}
-
-	//	else if (*clip != 2) {
-	//		auto one_min_x = (Vec2)xsimd::sub((xs)1.0, xn.value);
-	//		auto one_plus_x = (Vec2)xsimd::add((xs)1.0, xn.value);
-	//		auto pos_denom = (Vec2)1.0 + one_min_x * one_min_x * halfPi;
-	//		auto neg_denom = (Vec2)1.0 + one_plus_x * one_plus_x * halfPi;
-	//		auto y_pos = (Vec2)xsimd::div((xs) 1.0, (xs)pos_denom.value);
-	//		auto y_neg = (Vec2)xsimd::div((xs) -1.0, (xs)neg_denom.value);
-	//		yn = (y_pos & g_0 & ~l_1) + (y_neg & ~g_0 & ~g_n1);
-	//	}
-
-	//	else
-	//		yn = (Vec2)xsimd::sin((xs)xPi.value);
-
-	//	auto avgsum = yn + y_n1[channel];
-	//	auto avg = yn - xsimd::div((xs)avgsum.value, (xs)2.0);
-	//	if (k > 1.0)
-	//		yn -= avg * (Vec2)(1.0 - 1.0 / k);
-	//	else if (k < 1.0)
-	//		yn += avg * (Vec2)(1.0 - k);
-
-	//	x_n2[channel] = x_n1[channel];
-	//	x_n1[channel] = xn;
-	//	y_n1[channel] = yn;
-
-	//	return yn;
-	//}
-
 	template <typename T>
 	inline T processSample(int channel, T xn) noexcept
 	{
 		T yn, x1, x2;
-		double k = *curve;
-		if (*boost)
+		double k = curve_m;
+		if (boost_m)
 			k *= k;
 
 		/*DC offset for asym (need to ramp offset incase of switch)*/
-		xn += *type == 1 ? 0.1 : 0.0;
+		xn += type_m == 1 ? 0.1 : 0.0;
 
 		/*if we're in regular bounds, apply curve algo*/
 
@@ -163,11 +92,11 @@ public:
 		}
 
 		/*check for clip*/
-		else if (*clip == 1) {
+		else if (clip_m == 1) {
 			yn = jlimit((T) -1.0, (T) 1.0, xn);
 		}
 		/*single-period for finite type*/
-		else if (*clip != 2) {
+		else if (clip_m != 2) {
 			if (xn >= 0.0)
 				//yn = pow(1.0 / cosh(xn - 1.0), pi);
 				yn = 1.0 / (1.0 + (1.0 - xn) * (1.0 - xn) * halfPi);
