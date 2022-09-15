23a82ff5fe114a14f4ecbedda7cb97ccb4b929fd
Arboreal Audio <arborealaudio@protonmail.com>
2022-09-15 11:51:37 -0400

    template LR filter

--- Source/Processors/LRFilter.h
+++ Source/Processors/LRFilter.h
@@ -2,6 +2,7 @@
 
 /*custom Linkwitz-Riley filter object which contains two independent filter objects for the purpose
 of properly cascading band-passed outputs*/
+template <typename T>
 class LRFilter
 {
 public:
@@ -32,23 +33,19 @@ public:
 			band.reset();
 	}
 
-
 	/*processes the first filter object and returns the high-pass output*/
-	template <typename T>
 	void processSample(int channel, T inputSample, T& outputHigh)
 	{
 		outputHigh = bands[0].processSample(channel, inputSample);
 	}
 
 	/*processes the first filter object and returns the low- and high-pass outputs*/
-	template <typename T>
 	void processSample(int channel, T inputSample, T& outputLow, T& outputHigh)
 	{
 		bands[0].processSample(channel, inputSample, outputLow, outputHigh);
 	}
 
 	/*processes two inputs and returns two outputs, where the low output overwrites the first input, high output the second input*/
-	template <typename T>
 	void processTwoSamples(int channel, T input1, T input2, T& outputLow, T& outputHigh)
 	{
 		outputHigh = bands[0].processSample(channel, input2);
@@ -60,7 +57,7 @@ private:
 #if USE_SIMD_SAT
 	std::array<dsp::LinkwitzRileyFilter<dsp::SIMDRegister<float>>, 4> bands;
 #else
-	std::array<dsp::LinkwitzRileyFilter<float>, 2> bands;
+	std::array<dsp::LinkwitzRileyFilter<T>, 2> bands;
 #endif
 	double lastSampleRate = 0.0;
 
