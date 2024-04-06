549211502cb7c9ef7e0ff9167766dd8760d46b93
ArborealAudio <arborealaudio@protonmail.com>
2024-04-06 15:10:12 -0400

    Pull PFFFT with FetchContent

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,6 +5,7 @@ function(fetch_dependencies)
   FetchContent_Declare(juce
     GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
     GIT_TAG 7.0.11
+    GIT_SHALLOW TRUE
   )
   FetchContent_MakeAvailable(juce)
 
@@ -13,5 +14,13 @@ function(fetch_dependencies)
     GIT_TAG 8d0754f5d6ca1e95bc207b7743c04ebd7dc17e88
   )
   FetchContent_MakeAvailable(clap-juce-extensions)
+
+  FetchContent_Declare(pffft
+    GIT_REPOSITORY https://github.com/marton78/pffft.git
+    GIT_TAG e0bf595c98ded55cc457a371c1b29c8cab552628
+    GIT_SUBMODULES ""
+    GIT_SHALLOW TRUE
+  )
+  FetchContent_MakeAvailable(pffft)
   
 endfunction()
