a70a44aaed0e601572dae846bd54a1ef9b5dcdec
Arboreal Audio <arborealaudio@protonmail.com>
2024-04-06 13:02:10 -0400

    Fix clap deps

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -10,7 +10,7 @@ function(fetch_dependencies)
 
   FetchContent_Declare(clap-juce-extensions
     GIT_REPOSITORY https://github.com/free-audio/clap-juce-extensions.git
-    GIT_TAG main
+    GIT_TAG 8d0754f5d6ca1e95bc207b7743c04ebd7dc17e88
   )
   FetchContent_MakeAvailable(clap-juce-extensions)
   
