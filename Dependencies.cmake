f90ff136aadbba0691cda86d67b7cc1fe4fef20f
ArborealAudio <arborealaudio@protonmail.com>
2024-05-30 10:21:59 -0400

    Use custom JUCE fork w/ PFFFT support

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -3,14 +3,14 @@ function(fetch_dependencies)
   set(FETCHCONTENT_QUIET OFF)
 
   FetchContent_Declare(juce
-    GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
-    GIT_TAG 7.0.11
-    GIT_SHALLOW TRUE
+	  GIT_REPOSITORY https://github.com/ArborealAudio/JUCE.git
+	  GIT_TAG 730669c40166d49b3d2c9d6048cd0493cef998f8
+	  GIT_SHALLOW TRUE
   )
   FetchContent_MakeAvailable(juce)
 
   FetchContent_Declare(clap-juce-extensions
-    GIT_REPOSITORY https://github.com/free-audio/clap-juce-extensions.git
+	GIT_REPOSITORY https://github.com/free-audio/clap-juce-extensions.git
     GIT_TAG 8d0754f5d6ca1e95bc207b7743c04ebd7dc17e88
   )
   FetchContent_MakeAvailable(clap-juce-extensions)
