81bed85711ba14142e94f80c5e6f2418acc728d1
abriccio <alexbriccio@gmail.com>
2024-06-14 06:53:02 -0400

    Try 7zip on MacOS (they gotta have it) [ci skip]
    - Fix Linux tarball location
    - Copy windows installer resources before changing directory

--- installer/win-installer.sh
+++ installer/win-installer.sh
@@ -62,10 +62,11 @@ if [[ $presets == true ]]; then
     cp -r presets pkg/Factory
 fi
 cp LICENSE pkg/LICENSE
+cp ./installer/win-extra/install_script.iss pkg
+cp ./installer/win-extra/icon.ico pkg
 
 echo "Finishing installer package..."
 cd pkg
-cp $script_dir/win-extra/install_script.iss .
 
 # remove CLAP lines if not packaging it
 if [[ $clap == false ]]; then
@@ -75,7 +76,6 @@ fi
 
 [[ $presets == false ]] && sed -i '/Presets/d' install_script.iss
 
-cp $script_dir/win-extra/icon.ico .
 iscc -Dname=$name -Dversion=$version_num install_script.iss
 
 echo "Installer built in $PWD/Output"
